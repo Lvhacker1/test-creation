85998928d629bf0c4ad2ffcaf58caad028076264
Lvhacker1 <lavasha2@hotmail.com>
2025-09-04 16:10:53 +0200

    create navlinks

---
+++ src/data/data.ts
@@ -0,0 +1,7 @@
+import { NavLinks } from "@/types/types";
+
+export const navLinks: NavLinks[] = [
+    { name: "Home", href: "/" },
+    { name: "Courses", href: "/courses" },
+    { name: "About", href: "/about" },
+];
