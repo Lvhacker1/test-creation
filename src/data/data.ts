3a36581f4624f2cc20b70a221b2fb489302bb877
Lvhacker1 <lavasha2@hotmail.com>
2025-09-04 21:27:13 +0200

    add types and data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,18 @@
-import { NavLinks } from "@/types/types";
+import { CodeBlockData, NavLinks } from "@/types/types";
 
 export const navLinks: NavLinks[] = [
     { name: "Home", href: "/" },
     { name: "Courses", href: "/courses" },
     { name: "About", href: "/about" },
+];
+
+export const codeBlockData: CodeBlockData[] = [
+    { code: "console.log('Hello, world!');", language: "javascript" },
+    { code: "", language: "" },
+    { code: "", language: "" },
+    { code: "", language: "" },
+    { code: "", language: "" },
+    { code: "", language: "" },
+    { code: "", language: "" },
+    { code: "", language: "" },
 ];
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,14 @@ export interface NavLinks {
     name: string;
     href: string;
 }
+
+export interface CodeBlockData {
+    code: string;
+    language: string;
+}
+
+export interface CodeBlockProps {
+    code: string;
+    language: string;
+    onRun?: () => void;
+}
