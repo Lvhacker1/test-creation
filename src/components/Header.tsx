0784ce823bc098fb4c04e6b4cc0e5a0f8a54bb3d
Lvhacker1 <lavasha2@hotmail.com>
2025-09-04 16:31:41 +0200

    add navlinks to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,14 @@
+import { navLinks } from "@/data/data";
+import Link from "next/link";
+
 const Header = () => {
     return (
-        <header>
+        <header className="p-4 border-b border-b-gray-300 flex justify-around items-center">
+            <nav className="space-x-5 text-lg font-medium md:space-x-10">
+                {navLinks.map(link => 
+                    <Link key={link.href} href={link.href }>{link.name}</Link>
+                )}
+            </nav>
 
         </header>
     )
